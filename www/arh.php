53429f716fdc255b6cb5a19dc707f4d59bc9d627
Sergej Kukharev <sergej.kucharev@gmail.com>
2015-11-03 23:02:48 +0200

    0.0.1

---
+++ www/arh.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" >
+	<title>Стародавня Греція</title>
+	<link type"text/css" rel="stylesheet" href="/css/style.css" >
+	<link type"text/css" rel="stylesheet" href="/css/style-section.css" >
+</head>
+
+<body>
+	<section id="section1">
+		<table width="100%" border="1">
+	  	<tr>
+				<th bgcolor="#F1FFDF" align="center">
+					<a href="/Afini.html" style="text-decoration: none"><p><h3><strong>Афіни</strong></h3></p></a>
+					<a href="/Saloniki.html" style="text-decoration: none"><p><h3><strong>Салоніки<h3></strong></p></a>
+					<a href="/Sparta.html" style="text-decoration: none"><p><h3><strong>Спарта</h3></strong></p></a>
+					<a href="/Iraklion.html" style="text-decoration: none"><p><h3><strong>Іракліон</h3></strong></p></a>
+					<a href="/Fivi.html" style="text-decoration: none"><p><h3><strong>Фіви</h3></strong></p></a>
+				</th>
+			</tr>
+		</table>
+	</section>
+	<?php include('parts/header.php'); ?>
+	<?php include('parts/footer.php'); ?>
+</body>
+
+</html>
---
+++ www/parts/footer.php
@@ -0,0 +1,4 @@
+
+<footer>
+	© Copyright @ 2015  Vladislav Samusenko. Kyiv. NTUU "KPI"
+</footer>
